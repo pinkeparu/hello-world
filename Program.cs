7a9c5e544a6cc1d8982f53aadd73949bdd22a2ac
pinkeparu <pinkeparu@live.com>
2016-04-28 10:26:22 +0530

    App config

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayNonNull
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Enumerable.Repeat(100, 10).ToArray();
+
+            
+
+            // array
+        }
+    }
+}
